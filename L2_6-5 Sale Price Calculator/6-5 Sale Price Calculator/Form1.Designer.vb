4b5cf191ebea8761bca90d6c72ab12eedb27a277
LEKtopia <24890955+LEKtopia@users.noreply.github.com>
2019-01-20 21:54:12 -0600

    Initial commit - add files via upload

---
+++ L2_6-5 Sale Price Calculator/6-5 Sale Price Calculator/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblRetailPrice = New System.Windows.Forms.Label()
+        Me.lblDiscountPercent = New System.Windows.Forms.Label()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.txtRetailPrice = New System.Windows.Forms.TextBox()
+        Me.txtDiscountPercent = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblSalePrice = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblRetailPrice
+        '
+        Me.lblRetailPrice.AutoSize = True
+        Me.lblRetailPrice.Location = New System.Drawing.Point(118, 34)
+        Me.lblRetailPrice.Name = "lblRetailPrice"
+        Me.lblRetailPrice.Size = New System.Drawing.Size(64, 13)
+        Me.lblRetailPrice.TabIndex = 0
+        Me.lblRetailPrice.Text = "Retail Price:"
+        '
+        'lblDiscountPercent
+        '
+        Me.lblDiscountPercent.AutoSize = True
+        Me.lblDiscountPercent.Location = New System.Drawing.Point(90, 70)
+        Me.lblDiscountPercent.Name = "lblDiscountPercent"
+        Me.lblDiscountPercent.Size = New System.Drawing.Size(92, 13)
+        Me.lblDiscountPercent.TabIndex = 1
+        Me.lblDiscountPercent.Text = "Discount Percent:"
+        '
+        'lblMessage
+        '
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(42, 144)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(297, 23)
+        Me.lblMessage.TabIndex = 3
+        '
+        'txtRetailPrice
+        '
+        Me.txtRetailPrice.Location = New System.Drawing.Point(188, 31)
+        Me.txtRetailPrice.Name = "txtRetailPrice"
+        Me.txtRetailPrice.Size = New System.Drawing.Size(129, 20)
+        Me.txtRetailPrice.TabIndex = 4
+        '
+        'txtDiscountPercent
+        '
+        Me.txtDiscountPercent.Location = New System.Drawing.Point(188, 67)
+        Me.txtDiscountPercent.Name = "txtDiscountPercent"
+        Me.txtDiscountPercent.Size = New System.Drawing.Size(129, 20)
+        Me.txtDiscountPercent.TabIndex = 6
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(93, 188)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(138, 23)
+        Me.btnCalculate.TabIndex = 7
+        Me.btnCalculate.Text = "Calculate Sale Price"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(260, 188)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(68, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblSalePrice
+        '
+        Me.lblSalePrice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSalePrice.Location = New System.Drawing.Point(188, 107)
+        Me.lblSalePrice.Name = "lblSalePrice"
+        Me.lblSalePrice.Size = New System.Drawing.Size(129, 23)
+        Me.lblSalePrice.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(124, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Sale Price:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 231)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblSalePrice)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtDiscountPercent)
+        Me.Controls.Add(Me.txtRetailPrice)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.lblDiscountPercent)
+        Me.Controls.Add(Me.lblRetailPrice)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblRetailPrice As Label
+    Friend WithEvents lblDiscountPercent As Label
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents txtRetailPrice As TextBox
+    Friend WithEvents txtDiscountPercent As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblSalePrice As Label
+    Friend WithEvents Label1 As Label
+End Class
